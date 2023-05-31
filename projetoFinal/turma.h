e8fbd1faab3fb19907159cf6eca7a4a45d8de513
Leonardo Rafael <101893849+LeorafaelART@users.noreply.github.com>
2023-05-31 15:55:12 -0300

    Update turma.h

--- projetoFinal/turma.h
+++ projetoFinal/turma.h
@@ -1,11 +1,28 @@
 #ifndef TURMA_H
 #define TURMA_H
+#include "disciplina.h"
+#include <iostream>
 
-
-class turma
+class turma:public disciplina
 {
-public:
-    turma();
+    private:
+       std::string cod_turma;
+       int sub_turma;
+       int maxAlunos;
+       int numAlunos;
+    public:
+       turma();
+       const std::string &getCod_turma() const;
+       void setCod_turma(const std::string &newCod_turma);
+
+       int getSub_turma() const;
+       void setSub_turma(int newSub_turma);
+
+       int getMaxAlunos() const;
+       void setMaxAlunos(int newMaxAlunos);
+
+       int getNumAlunos() const;
+       void setNumAlunos(int newNumAlunos);
 };
 
 #endif // TURMA_H
