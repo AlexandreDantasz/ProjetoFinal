3a348688653e831d7c841c4d4bbff2d637678a5c
Alexandre Dantas <102476384+AlexandreDantasz@users.noreply.github.com>
2023-05-31 16:08:51 -0300

    versão 1 da disciplina.h

--- projetoFinal/disciplina.h
+++ projetoFinal/disciplina.h
@@ -1,11 +1,18 @@
 #ifndef DISCIPLINA_H
 #define DISCIPLINA_H
+#include <QString>
 
-
-class disciplina
-{
+class disciplina {
+private:
+    string cod_disciplina;
+    string nome_disciplina;
 public:
     disciplina();
+    QString toQString();
+    string getCod_disciplina() const;
+    void setCod_disciplina(const string &newCod_disciplina);
+    string getNome_disciplina() const;
+    void setNome_disciplina(const string &newNome_disciplina);
 };
 
 #endif // DISCIPLINA_H
