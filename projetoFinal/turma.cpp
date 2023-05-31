58fe9cc85815113ea43dd3b5665c50dfd15bbb52
Leonardo Rafael <101893849+LeorafaelART@users.noreply.github.com>
2023-05-31 15:57:24 -0300

    Update turma.cpp

--- projetoFinal/turma.cpp
+++ projetoFinal/turma.cpp
@@ -1,5 +1,45 @@
 #include "turma.h"
 
+const std::string &turma::getCod_turma() const
+{
+    return cod_turma;
+}
+
+void turma::setCod_turma(const std::string &newCod_turma)
+{
+    cod_turma = newCod_turma;
+}
+
+int turma::getSub_turma() const
+{
+    return sub_turma;
+}
+
+void turma::setSub_turma(int newSub_turma)
+{
+    sub_turma = newSub_turma;
+}
+
+int turma::getMaxAlunos() const
+{
+    return maxAlunos;
+}
+
+void turma::setMaxAlunos(int newMaxAlunos)
+{
+    maxAlunos = newMaxAlunos;
+}
+
+int turma::getNumAlunos() const
+{
+    return numAlunos;
+}
+
+void turma::setNumAlunos(int newNumAlunos)
+{
+    numAlunos = newNumAlunos;
+}
+
 turma::turma()
 {
 
